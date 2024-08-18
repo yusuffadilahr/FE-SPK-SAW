7ba4cd9ee0ae8e5afd6d8fd2b10bed3754c65e6c
yusuffadilahr <yusuffadilah58@gmail.com>
2024-08-18 22:20:24 +0700

    done lagi

--- src/components/element/navigation/sidebar/sidebar.jsx
+++ src/components/element/navigation/sidebar/sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import SidebarLinks from './sidebarLinks'
 import Aicons from '../../icons/AIcons'
 import Bicons from '../../icons/Bicons'
@@ -10,10 +10,13 @@ import Dicons from '../../icons/Dicons'
 import ProfileIcons from '../../icons/profileIcons'
 
 const Sidebar = () => {
+    const location = useLocation()
 
     const handleNavigation = () => {
         return localStorage.getItem('role')
     }
+
+    const isActive = (url) => location.pathname === url
     return (
         <div className='fixed top-0 left-0 h-screen w-1/6 bg-gray-50 border-r z-30'>
             <div className='w-full bg-blue-400 h-20'>
@@ -29,43 +32,43 @@ const Sidebar = () => {
                     <ul>
                         {handleNavigation() === 'admin' ?
                             <>
-                                <SidebarLinks url='/dashboard-admin' title='Dashboard'>
+                                <SidebarLinks url='/dashboard-admin' title='Dashboard' className={isActive('/dashboard-admin') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex pl-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <HomeIcons />
                                 </SidebarLinks>
-                                <SidebarLinks url='/data-alternatif-admin' title='Data Alternatif'>
+                                <SidebarLinks url='/data-alternatif-admin' title='Data Alternatif' className={isActive('/data-alternatif-admin') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <Aicons />
                                 </SidebarLinks>
-                                <SidebarLinks url='/data-kriteria-admin' title='Data Kriteria'>
+                                <SidebarLinks url='/data-kriteria-admin' title='Data Kriteria' className={isActive('/data-kriteria-admin') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <Bicons />
                                 </SidebarLinks>
-                                <SidebarLinks url='/penilaian-alternatif-admin' title='Penilaian Alternatif'>
+                                <SidebarLinks url='/penilaian-alternatif-admin' title='Penilaian Alternatif' className={isActive('/penilaian-alternatif-admin') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <Cicons />
                                 </SidebarLinks>
-                                <SidebarLinks url='/proses-perhitungan-admin' title='Proses Perhitungan'>
+                                <SidebarLinks url='/proses-perhitungan-admin' title='Proses Perhitungan' className={isActive('/proses-perhitungan-admin') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <Dicons />
                                 </SidebarLinks>
-                                <SidebarLinks url='/data-hasil-admin' title='Data Hasil Keputusan'>
+                                <SidebarLinks url='/data-hasil-admin' title='Data Hasil Keputusan' className={isActive('/data-hasil-admin') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <Eicons />
                                 </SidebarLinks>
                             </>
                             :
                             <>
-                                <SidebarLinks url='/dashboard-user' title='Dashboard'>
+                                <SidebarLinks url='/dashboard-user' title='Dashboard' className={isActive('/dashboard-user') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <HomeIcons />
                                 </SidebarLinks>
-                                <SidebarLinks url='/data-alternatif-user' title='Data Alternatif'>
+                                <SidebarLinks url='/data-alternatif-user' title='Data Alternatif' className={isActive('/data-alternatif-user') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <Aicons />
                                 </SidebarLinks>
-                                <SidebarLinks url='/data-kriteria-user' title='Data Kriteria'>
+                                <SidebarLinks url='/data-kriteria-user' title='Data Kriteria' className={isActive('/data-kriteria-user') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <Bicons />
                                 </SidebarLinks>
-                                <SidebarLinks url='/penilaian-alternatif-user' title='Penilaian Alternatif'>
+                                <SidebarLinks url='/penilaian-alternatif-user' title='Penilaian Alternatif' className={isActive('/penilaian-alternatif-user') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <Cicons />
                                 </SidebarLinks>
-                                <SidebarLinks url='/proses-perhitungan-user' title='Proses Perhitungan '>
+                                <SidebarLinks url='/proses-perhitungan-user' title='Proses Perhitungan ' className={isActive('/proses-perhitungan-user') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <Dicons />
                                 </SidebarLinks>
-                                <SidebarLinks url='/data-hasil-user' title='Data Hasil Keputusan'>
+                                <SidebarLinks url='/data-hasil-user' title='Data Hasil Keputusan' className={isActive('/data-hasil-user') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                     <Eicons />
                                 </SidebarLinks>
                             </>
@@ -78,13 +81,13 @@ const Sidebar = () => {
                         {handleNavigation() === "admin" ?
                             <>
                                 <Link to='/users'>
-                                    <li className='pb-8 flex items-center ml-2'>
+                                    <li className={isActive('/users') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                         <ProfileIcons />
                                         <h1 className='ml-3 text-sm text-gray-700'>Profile</h1>
                                     </li>
                                 </Link>
                                 <Link to='https://www.instagram.com/combuchacoffee/?igshid=YmMyMTA2M2Y%3D'>
-                                    <li className='pb-8 flex items-center ml-2'>
+                                    <li className={isActive('https://www.instagram.com/combuchacoffee/?igshid=YmMyMTA2M2Y%3D') ? 'bg-slate-100 shadow py-2 mb-7 flex pl-2 text-gray-600 items-center hover:text-gray-400' : 'mb-7 flex ml-2 items-center text-gray-700 hover:text-gray-400'}>
                                         <ProfileIcons />
                                         <h1 className='ml-3 text-sm text-gray-700'>Combucha Coffee</h1>
                                     </li>
--- src/components/element/navigation/sidebar/sidebarLinks.jsx
+++ src/components/element/navigation/sidebar/sidebarLinks.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const SidebarLinks = (props) => {
-    const {url, title, children}=props
+const SidebarLinks = ({url, title, children, className='pb-8 flex ml-2 items-center text-gray-700 hover:text-gray-400'}) => {
     return (
         <Link to={url}>
-            <li className='pb-8 flex ml-2 items-center text-gray-700 hover:text-gray-400'>
+            <li className={className}>
                 {children}
                 <h1 className='ml-3 text-sm'>{title}</h1>
             </li>
